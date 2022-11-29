130be2ff3b394d5834dd6b93323bd343c0494011
Naruto TAKAHASHI <tnaruto@gmail.com>
2022-11-30 00:46:04 +0900

    update android

--- Cocos2d-x_v3/scripts/prepare_mac.sh
+++ Cocos2d-x_v3/scripts/prepare_mac.sh
@@ -19,7 +19,8 @@ fi
 # download cocos2d-x depends on 3rd party libraries
 MAJOR_VER=$(sw_vers -productVersion | cut -d'.' -f1)
 if [ "$MAJOR_VER" -ge "12" ]; then
-    sed -i.bak -e 's/python/python3/g' ./download-deps.py
+    git checkout ./download-deps.py
+    sed -i.bak -e 's|#!/usr/bin/env python|#!/usr/bin/env python3|g' ./download-deps.py
 fi
 ./download-deps.py -r yes
 popd > /dev/null # ${ROOTDIR}/cocos2d
